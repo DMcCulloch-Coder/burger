9ba161a8836bf7727d9359777f30ccc226bad17c
David <DMcCulloch86@gmail.com>
2019-12-18 14:23:38 -0500

    made model

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,21 @@
 const orm = require('../config/orm');
 
+const burger = {
+    all: (cb) => {
+        orm.selectAll('burgers', (res) => {
+            cb(res)
+        })
+    },
+    insert: (name, cb) => {
+        orm.insertOne('burgers', name, (res) => {
+            cb(res)
+        })
+    },
+    update: (id, cb) => {
+        orm.selectAll('burgers', id, (res) => {
+            cb(res)
+        })
+    }
+}
 
+module.exports = burger
