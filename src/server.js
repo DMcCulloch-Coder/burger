9c047edebd78513746e5372f9d482b81965e6716
David <DMcCulloch86@gmail.com>
2019-12-18 13:21:31 -0500

    updated database, wrote up orm, started working on views and server

--- src/server.js
+++ src/server.js
@@ -3,3 +3,19 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+const routes = require("../controllers/burgers_controller")
+
+const PORT = process.env.PORT || 3000;
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}`)
+});
